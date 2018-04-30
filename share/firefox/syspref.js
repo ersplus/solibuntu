c2e6f1623cf445d3ffa2e48c868a75d3d181e45b
Ersplus <ersplus@users.noreply.github.com>
2018-04-30 19:30:57 +0200

    Update syspref.js

--- share/firefox/syspref.js
+++ share/firefox/syspref.js
@@ -1,1 +1,24 @@
-user_pref("browser.startup.homepage", "https://start.duckduckgo.com/?kl=fr-fr"); // page d'accueil = recherche DuckDuckGo en français
+//
+// Fichier de configuation de Firefox
+// Pour générer facilement un fichier de congig :
+// cat $HOME/.mozilla/firefox/*.default/prefs.js
+// Prefixe de configuration :
+// lockpref ou pref
+//
+// Page de démarrage
+//
+user_pref("browser.startup.homepage", "http://google.fr");
+//
+// Configuration du proxy
+//
+lockPref("network.proxy.http","address.of.proxy");
+lockPref("network.proxy.http_port",80);
+lockPref("network.proxy.ssl","address.of.proxy");
+lockPref("network.proxy.ssl_port",80);
+lockPref("network.proxy.type",1);
+lockPref("network.proxy.share_proxy_settings", true)
+//
+// Gestion des données personnelles
+//
+lockPref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
