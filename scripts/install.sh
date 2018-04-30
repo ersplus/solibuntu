91e5c4376d42c6ec027b256fffd7692d31a07eb4
Ersplus <ersplus@users.noreply.github.com>
2018-04-30 18:51:02 +0200

    Désinstallation de plugins Thunar et Installation de programmes
    Désinstallation de plugins Thunar et Installation de Xsane

--- scripts/install.sh
+++ scripts/install.sh
@@ -49,13 +49,14 @@ cp -f $repinstallation/scripts/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lig
 # Création du compte gestionnaire
 #-------------------------------------------------------
 
+echo "Création des utilisateurs"
+
 # Ajout du compte gestionnaire Solibuntu
 useradd -m gestionnaire
 echo -e "AdminAsso\nAdminAsso" | passwd gestionnaire
 usermod -c "Gestionnaire Solibuntu" gestionnaire
 
 # Modification du compte administrateur Solibuntu
-echo "Création des utilisateurs"
 echo "Modification du compte administtrateur"
 # La modification du compte administrateur ne peut se faire dans la session administrateur
 # usermod -c "Administrateur Solibuntu" administrateur
@@ -78,13 +79,12 @@ apt full-upgrade -y && apt install -f && apt-get clean
 
 # Suppression des applications
 apt remove synapse seahorse thunderbird transmission-* pidgin xfce4-notes xfce4-mailwatch-plugin xfce4-weather-plugin -y
+
 # Les jeux
 apt remove sgt-launcher sgt-puzzles gnome-sudoku gnome-mines -y
 
-# Chrome n'est plus le navigateur de Solibuntu
-
 # Installation des applications complémentaires
-apt install exfat-utils gksu feh yad -y
+apt install exfat-utils gksu feh yad imagemagick xsane -y
 
 # Installation des polices complémentaires
 apt install -y gsfonts gsfonts-other gsfonts-x11 ttf-mscorefonts-installer t1-xfree86-nonfree ttf-alee ttf-ancient-fonts ttf-arabeyes fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp ttf-atarismall fonts-bpg-georgian fonts-dustin fonts-f500 fonts-sil-gentium ttf-georgewilliams ttf-isabella fonts-larabie-deco fonts-larabie-straight fonts-larabie-uncommon ttf-sjfonts ttf-staypuft ttf-summersby fonts-ubuntu-title ttf-xfree86-nonfree xfonts-intl-european xfonts-jmk xfonts-terminus fonts-arphic-uming fonts-ipafont-mincho fonts-ipafont-gothic fonts-unfonts-core hplip cups-pdf exfat-utils chromium-browser imagemagick xsane
@@ -93,7 +93,10 @@ apt install -y gsfonts gsfonts-other gsfonts-x11 ttf-mscorefonts-installer t1-xf
 apt-get install -y hplip hplip-data hplip-doc hpijs-ppds hplip-gui printer-driver-hpcups printer-driver-hpijs printer-driver-pxljr 
 
 # Installation de Gdebi pour résoudre les dépendances de l'installation de CTparental
-apt-get install -y gdebi	
+apt-get install -y gdebi
+
+# Désinstallation des extensions de Thunar Ouvrir dans un terminal etc.
+dconf write /org/mate/caja/extensions/disabled-extensions "['libcaja-main-menu,'libcaja-sento','libcaja-python','libcaja-pythin','libcaja-wallpaper','libcaja-gksu','libcaja-engrampa','libcaja-open-terminal','libcatril-properties-page']"
 
 # hp-plugin -i
 
@@ -120,7 +123,7 @@ cp $repinstallation/scripts/lightdm.conf.d/* /etc/lightdm/lightdm.conf.d/
 #  Écran de connexion de la session invité
 #-------------------------------------------------------
 # Navigateur par défaut
-xdg-settings set default-web-browser chromium-browser.desktop
+# xdg-settings set default-web-browser chromium-browser.desktop
 
 
 echo "Fin de l'installation"
