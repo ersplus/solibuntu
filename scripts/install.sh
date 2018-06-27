73d7c47ce3d40f9b93ff22b1583589eaee84ae66
bastlenoob <bastien.labouche96@orange.fr>
2018-06-27 15:29:26 +0200

    Test copie fichier sudoers

--- scripts/install.sh
+++ scripts/install.sh
@@ -153,7 +153,7 @@ if [ $? == 0 ] ; then
 	# voir Xavier pour docs
 	xdg-settings set default-web-browser firefox-browser.desktop
 	cp -r $repinstallation/share/firefox/syspref.js /etc/firefox/syspref.js 
-
+	cp /opt/borne/share/sudoers /etc/sudoers
 	echo "Fin de l'installation"
 fi
 exit 0
