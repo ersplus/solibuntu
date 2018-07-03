626a842957b51d10da569b9ab0303bc9db9625b2
bastlenoob <bastien.labouche96@orange.fr>
2018-07-03 15:48:38 +0200

    Modif install

--- scripts/install.sh
+++ scripts/install.sh
@@ -67,7 +67,7 @@ if [ $? == 0 ] ; then
 
 	# Liaison vers le profil utilisateur
 	echo "Squelette environnement Invité"
-	ln -s /home/gestionnaire /etc/guest-session/skel
+	#ln -s /home/gestionnaire /etc/guest-session/skel
 
 	# Configuration Autologin et les scripts de lightdm
 	cp -f $repinstallation/scripts/lightdm.conf.d/50-logout-restoreinvite.conf /etc/lightdm/lightdm.conf.d/50-logout-restoreinvite.conf
