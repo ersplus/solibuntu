54f65209fb64e3e3d1ea2afca8f3de58fe93c008
bastlenoob <bastien.labouche96@orange.fr>
2018-07-05 08:44:44 +0200

    Test résolution bug sessions vides

--- scripts/sessionStart.sh
+++ scripts/sessionStart.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 touch /tmp/sessionstarted
+
+if [ ! -f $HOME/.config/solibuntu ] ; then
+	cp /opt/borne/share/config.tar.gz $HOME/
+	cd $HOME
+	rm -rf .config
+	tar -xvzf config.tar.gz
+	rm config.tar.gz
+fi
