c92921351b1896d14fe1e437097cb82db63bff7c
bastlenoob <bastien.labouche96@orange.fr>
2018-07-10 10:22:04 +0200

    Test résolution bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -174,6 +174,7 @@ if [ $? == 0 ] ; then
 	cd /home/administrateur/
 	rm -rf .config
 	tar -xvzf config.tar.gz
+	nohum xterm &
 	#rm config.tar.gz
 
 	#cp /opt/borne/share/config.tar.gz /home/gestionnaire/
--- scripts/sessionStart.sh
+++ scripts/sessionStart.sh
@@ -2,10 +2,10 @@
 touch /tmp/sessionstarted
 
 if [ ! -f $HOME/.config/solibuntu ] ; then
-	#cp /opt/borne/share/config.tar.gz $HOME/
-	#cd $HOME
-	#rm -rf .config
-	#tar -xvzf config.tar.gz
+	cp /opt/borne/share/config.tar.gz $HOME/
+	cd $HOME
+	rm -rf .config
+	tar -xvzf config.tar.gz
 	#rm config.tar.gz
 	if [ $USER == "gestionnaire" ] ; then
 		chown gestionnaire:gestionnaire ~/.config/
