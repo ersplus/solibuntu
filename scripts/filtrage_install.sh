e2838545193c67621da6b372d99958f30a655e73
bastlenoob <bastien.labouche96@orange.fr>
2018-07-18 10:43:19 +0200

    Ajout paquet deb de CTparental

--- scripts/filtrage_install.sh
+++ scripts/filtrage_install.sh
@@ -4,9 +4,11 @@
 # Installation filtrage
 #-------------------------------------------------------
 installFiltrage() {
-	wget wget https://gitlab.com/marsat/CTparental/uploads/53e32309e587aa7d61447d9f9adc9981/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb -O /opt/borne/share/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb
+	#wget wget https://gitlab.com/marsat/CTparental/uploads/53e32309e587aa7d61447d9f9adc9981/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb -O /opt/borne/share/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb
 	#sudo dpkg -i /opt/borne/share/ctparental_ubuntu16.04_4.21.06-1.0_all.deb
-	gdebi-gtk -n --auto-close /opt/borne/share/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb
+	#gdebi-gtk -n --auto-close /opt/borne/share/ctparental_debian9_ubuntu17.xx_18.04_4.22.07-1.0_all.deb
+	
+	gdebi-gtk -n --auto-close /opt/borne/share/ctparental.deb
 	return $?
 	#cp -rf /opt/borne/share/CTparental /usr/bin/CTparental
 }
