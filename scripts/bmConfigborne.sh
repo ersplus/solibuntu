b82e0db4ea1354f236bc5881ba1b538ece99d7fd
bastlenoob <bastien.labouche96@orange.fr>
2018-07-19 15:51:26 +0200

    Correction bug et test copie profils

--- scripts/bmConfigborne.sh
+++ scripts/bmConfigborne.sh
@@ -38,7 +38,7 @@ if [ $1 == "gestionnaire" ] ; then
 			--margins="400" \
 			true  "Arrêter l'ordinateur" \
 			false "Redémarrer l'ordinateur" \
-			false "Accéder au Bureau" \
+			false "Configurer l'ordinateur" \
 			false "Créer une clé USB")
 
 # Choix administrateur
