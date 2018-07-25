21a07fcb6c1479955919ff4fbe42b5d2e03b61fc
bastlenoob <bastien.labouche96@orange.fr>
2018-07-25 10:47:27 +0200

    Modification messages

--- scripts/filtrage_install.sh
+++ scripts/filtrage_install.sh
@@ -39,13 +39,17 @@ repinstallation="/opt/borne"
 	echo "70" ; sleep 1
 	#echo "# Configuation du proxy" ; sudo cp -rf /opt/borne/share/proxy/defaulton /etc/chromium-browser/default
 	echo "80" ; sleep 1
-	echo "# Le filtrage internet a été installé avec succès, le filtrage par défaut sera activé lors de l’utilisation de Solibuntu. Vous pourrez configurer celui-ci, si nécessaire, avec le compte administrateur et son mot de passe à l’adresse internet http://admin.ct.local" ;
+	echo "# Le filtrage internet a été installé avec succès, 
+le filtrage par défaut sera activé lors de l’utilisation de Solibuntu.
+ 
+Vous pourrez configurer celui-ci, si nécessaire, avec le compte administrateur et son mot de passe à l’adresse internet 
+http://admin.ct.local" ;
 	echo "99" ; sleep 1
 	)  |
 	zenity --progress \
 	  --title="Progression de installation" \
 	  --text="Installation du filtrage..." \
-	  --width=300 \
+	  --width=450 \
 	  --percentage=0
 
 	#if [ "$?" = -1 ] ; then
