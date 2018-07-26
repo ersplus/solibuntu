ad0fe30366b6b519ef51fde571261541c4f28852
bastlenoob <bastien.labouche96@orange.fr>
2018-07-26 09:26:36 +0200

    Modification installation filtrage et ajout option de supression

--- scripts/filtrage_remove.sh
+++ scripts/filtrage_remove.sh
@@ -19,6 +19,8 @@ if (test -f "/usr/sbin/dansguardian"); then
 			sudo rm -rf /etc/CTparental && rm -rf /etc/dansguardian && rm -rf /etc/squid
 			sudo apt-get autoremove	
 			sudo cp -rf /opt/borne/share/proxy/defaultoff /etc/chromium-browser/default
+			rm /etc/firefox/syspref.js
+			mv /etc/firefox/syspref.js.back /etc/firefox/syspref.js
 			# Message d'information
 			zenity --info="Le filtrage a été supprimé. L'ordinateur va redémarrer"
 			# Redémarrage
