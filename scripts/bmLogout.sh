e1ba868e0ced29b367f2e2e21d9d8785047e9327
Ersplus <ersplus@users.noreply.github.com>
2025-12-12 17:31:53 +0100

    Apply automatic lint and security fixes

--- scripts/bmLogout.sh
+++ scripts/bmLogout.sh
@@ -6,23 +6,22 @@
 
 repinstallation="/opt/borne"
 
-
 # On vérifie la valeur contenue dans le fichier lastadminlogin
 # Si 1 alors la dernière session était admin
 
-adminConnect=cat /root/.lastadminlogin
+adminConnect=$(cat /root/.lastadminlogin 2>/dev/null || echo "")
 
 # Test
 # Si la valeur est de 1 alors
 
-	if [ -f /root/.lastadminlogin ]; then
-		cp $repinstallation/scripts/lightdm.conf.d/* /etc/lightdm/lightdm.conf.d
-		echo "0" > /root/.lastadminlogin
-		pkill bmGreeter.sh
-		pkill lightdm
-		exit 1
-	else
-	zenity --info --text "Le fichier n'est pas la"
-	fi
+if [ -f "/root/.lastadminlogin" ]; then
+	cp -f "$repinstallation/scripts/lightdm.conf.d/"* /etc/lightdm/lightdm.conf.d/
+	echo "0" > /root/.lastadminlogin
+	pkill bmGreeter.sh
+	pkill lightdm
+	exit 1
+else
+	zenity --info --text "Le fichier n'est pas là"
+fi
 
 exit 0
