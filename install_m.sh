6e3fd42f82494b067a16cf50ac2352dad60d10a0
Borne Solibuntu <admin@solibuntu.local>
2025-12-14 11:23:47 +0100

    Mise à jour install_m.sh pour télécharger depuis les releases GitHub v0.5.0
    - Utilise le lien releases/latest/download au lieu de la branche
    - Simplifié pour installation LiveCD (pas de sauvegarde)
    - Messages informatifs améliorés

--- install_m.sh
+++ install_m.sh
@@ -1,31 +1,41 @@
 #! /bin/bash
 
-### Projet Solisol.org               ###
-### Solibuntu master                 ###
-### Installation Solibuntu master    ###
-### 28/07/2018                       ###
+### Projet Solisol.org                    ###
+### Solibuntu Xubuntu 24.04               ###
+### Installation Solibuntu Xubuntu 24.04  ###
+### Mis à jour : 14/12/2025               ###
 
 
 repinstallation="/opt/borne"
 
 #-------------------------------------------------------
-#  Réccupération des sources MAster du projet
+#  Récupération de la dernière release Xubuntu 24.04
 #-------------------------------------------------------
 
 cd /opt/
-# Check branche master
-wget https://github.com/ersplus/solibuntu/archive/master.zip -O /opt/master.zip
 
-if [ $? == 0 ] ; then
-	rm -rf /opt/borne
-	unzip master.zip
-	mv /opt/solibuntu-master $repinstallation
+# Télécharger la dernière release stable depuis GitHub
+echo "Téléchargement de la dernière version de Solibuntu Xubuntu 24.04..."
+wget https://github.com/ersplus/solibuntu/releases/latest/download/solibuntu-xubuntu-24.04.zip -O /opt/solibuntu-latest.zip
+
+if [ $? -eq 0 ]; then
+	echo "Installation en cours..."
+	
+	# Extraire et installer
+	unzip -q solibuntu-latest.zip
+	mv /opt/solibuntu-xubuntu-24.04 $repinstallation
 	chmod +x $repinstallation/scripts/*.sh
-	rm master.zip
+	rm solibuntu-latest.zip
 
+	# Lancer le script d'installation
 	cd /opt/borne/scripts
 	./install.sh $1
 
-	echo "Fin de l'installation"
+	echo "✓ Installation terminée avec succès"
+else
+	echo "✗ Erreur lors du téléchargement de Solibuntu"
+	echo "Vérifiez votre connexion Internet et que la release existe sur GitHub"
+	exit 1
 fi
+
 exit 0
